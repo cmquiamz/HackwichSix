37872196c0223140ac6cfc351dd0b6cfc4037127
CM Student <cmstudent@UHWO-ACM-20.local>
2018-03-12 10:47:33 -1000

    Finished the slider value game

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -13,16 +13,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     //create a variable that can hold the value of the slider
     var currentValue: Int = 0
+
+    //create a variable to hold the target value
+    var targetValue: Int = 0
+    
+    @IBOutlet var targetLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        //set currentValue to the initial value of the slider
+        currentValue = lroundf(slider.value)
+        
+        //Call startNewRound
+        startNewRound()
+        
+        //set constant variable to store the slider's image:
+        let thumbImageNormal = UIImage(named: "SliderThumbe-Normal")
+        slider.setThumbImage(thumbImageNormal, for : .normal)
 
+       
+    }
+    
+    func updateTargetLabel() {
+        targetLabel.text = String(targetValue)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateTargetLabel()
+    
+    
+    }
+    
   
     @IBAction func sliderHasMoved(_ sender: Any) {
         print("The value of the slider is:\(slider.value)")
@@ -32,7 +62,7 @@ class ViewController: UIViewController {
     
     @IBAction func myGuessButtonPressed(_ sender: Any) {
         //5. New variable message the displays a message that includes currentValue
-        let message = "The value is: \(currentValue)"
+        let message = "The value is: \(currentValue)" + "\nThe target value is: \(targetValue)"
         
         //1. create alert view
         let alert = UIAlertController(title:"Hello World!",message: message, preferredStyle: .alert)
@@ -45,7 +75,8 @@ class ViewController: UIViewController {
         
         //4. present alertview on the screen
         present(alert, animated: true, completion: nil)
-       
+        
+        startNewRound()
     }
     
 }
